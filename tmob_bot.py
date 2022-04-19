1d5b2cbf9ee0a9284280a3c9937ce446c8379a71
sage <soorajantony@live.in>
2022-04-19 17:51:30 +0530

    click patch

--- tmob_bot.py
+++ tmob_bot.py
@@ -316,10 +316,10 @@ def core(mobileNums, tmob_username, tmob_password, imap_url, imap_password, imap
         try:
             driver.find_element(by=By.ID, value="lineMeatBall0").find_elements(by=By.TAG_NAME, value="li")[3].click()
             time.sleep(1.2*5)
-            driver.find_element_by_id("managePopUp").find_elements_by_class_name("mat-checkbox-inner-container")[
+            driver.find_element(by=By.ID,value="managePopUp").find_elements_by_class_name("mat-checkbox-inner-container")[
                 0].find_elements_by_tag_name("input")[0].send_keys(" ")
             time.sleep(1.2*3)
-            driver.find_element_by_id("managePopUp").find_elements(by=By.TAG_NAME, value="button")[-1].click()
+            driver.find_element(by=By.ID,value="managePopUp").find_elements(by=By.TAG_NAME, value="button")[-1].click()
             time.sleep(1.2*10)
             if "Restore-line request complete" in driver.page_source:
                 logger.debug("Request generated for " + mobileNum + " with transaction number: " + driver.find_element(by=By.ID, value="old-number").text)
